5e65d3cac59c71c73bbfe37e1d1356b18515a858
JDCrespo <josedomingo26196@gmail.com>
2019-07-03 08:31:25 +0200

    Update and rename edit.blade.php to create.blade.php

--- resources/views/tipoTweet/create.blade.php
+++ resources/views/tipoTweet/create.blade.php
@@ -1,12 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<form action="/tipoTweet" method="POST">
+<form action="{{ route('tipoTweet.store') }}" method="POST">
 	@csrf	
 	<textarea maxlength="240" name="contenido" id="contenido" placeholder="Contenido del tweet" value="{{ old('contenido') }}"></textarea>
-	@error('contenido')
-	    <div class="alert alert-danger">{{ $message }}</div>
-	@enderror
+	{!! $errors->first('contenido', "
+	    <div class="alert alert-danger">:message</div>") !!}
 	<input class="btn btn-success" type="submit" value="GUARDAR">
 </form>
 @endsection
