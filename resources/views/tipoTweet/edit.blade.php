811644e91e7925fc479b1e3702d8b336936d3885
JDCrespo <josedomingo26196@gmail.com>
2019-07-03 08:32:56 +0200

    Create edit.blade.php

---
+++ resources/views/tipoTweet/edit.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<form action="{{ route('tipoTweet.update', $tipoTweet->id) }}" method="POST">
+  {{ method_field('PUT') }}
+	@csrf	
+	<textarea maxlength="240" name="contenido" id="contenido" placeholder="Contenido del tweet" value="{{ $tipoTweet->contenido }}"></textarea>
+	{!! $errors->first('contenido', "
+	    <div class="alert alert-danger">:message</div>") !!}
+	<input class="btn btn-success" type="submit" value="GUARDAR">
+</form>
+@endsection
