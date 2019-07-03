eb23f5479eeffa5eb030347fb30214acd7ed6bef
JDCrespo <josedomingo26196@gmail.com>
2019-07-03 12:00:38 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/muertes', 'MuertesController@index')->name('muertes');
 
 Route::resource("tipoTweet", "TipoTweetController");
 
